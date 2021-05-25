bcd31565dfeb345dab217f703f82e5818d6d1bce
JorgeI40 <leon.jorge@tec.mx>
2021-05-25 10:36:02 -0500

    Cambiar la URL del REP

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef __SIMPLE_BOX_VERSION_H__
 #define __SIMPLE_BOX_VERSION_H__
 
-#define SIMPLE_BOX_VERSION "SimpleBox v0.2.0"
+#define SIMPLE_BOX_VERSION "SimpleBox v0.2.1"
 
 #endif __SIMPLE_BOX_H__
